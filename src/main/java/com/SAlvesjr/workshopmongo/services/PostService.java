44729afc6fcb38665794519ef589214e3c3f40bd
Sergio Alves <sergio.alves@dce.ufpb.br>
2020-01-22 21:50:26 -0300

    Consulta com vários critérios

--- src/main/java/com/SAlvesjr/workshopmongo/services/PostService.java
+++ src/main/java/com/SAlvesjr/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.SAlvesjr.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,20 +13,25 @@ import com.SAlvesjr.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
-	
+
 	@Autowired
 	private PostRepository repo;
-	
-	public List<Post> findAll(){
+
+	public List<Post> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-	}		
-	
-	public List<Post> findByTitle(String text){
+	}
+
+	public List<Post> findByTitle(String text) {
 		return repo.findByTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
